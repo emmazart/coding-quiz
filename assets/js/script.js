4f31f6dae4b39005f429a4e05290677f5176b27a
Emma Olson <emma.olson794@gmail.com>
2022-03-28 11:24:54 -0500

    basic html and css framing

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// on click of start button
+// timer starts
+// first question pops up
+
+
+// while timer > 1 and number of questions < 10 (or whatever)
+
+// on click of any button, next question loads
+// if previous question is right (if button value = correct answer)
+// else 
+
+
+
+//use localstorage to log highscores
